4cbb96872d5f2deeb6d284b45bcedcc2e379db23
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-26 09:44:45 +0530

    Backend Setup

--- Frontend/intelliview_ai/src/pages/LandingPage.jsx
+++ Frontend/intelliview_ai/src/pages/LandingPage.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 
+import HERO_IMG from "../assets/hero-img.png";
+import { APP_FEATURES } from "../utils/data";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
 const LandingPage = () => {
+    const navigate = useNavigate();
+    
+    const [openAuthModal, setOpenAuthModal] = useState(false);
+    const [currentPage, setCurrentPage] = useState("login");
+
+    const handleCTA = () => {};
+    
     return (
         <div>LandingPage</div>
     )
