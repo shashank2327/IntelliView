230af6a986befc5501ec5e8562e749ee229232fd
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-27 19:16:44 +0530

    auth routh and controller setup

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+
+
+//routes import
+const authRoutes = require('./routes/authRoutes')
+const sessionRoutes = require('./routes/sessionRoutes')
+const questionRoutes = require('./routes/questionRoutes')
+
+
 
 const app = express();
 
@@ -12,9 +21,22 @@ app.use(cors({
 }));
 
 
+connectDB()
+
+// middleware
 app.use(express.json());
 
+// routes declaration
+app.use("/api/auth", authRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/questions", questionRoutes);
+
+
+
+// serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
+
+// server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
