f8fdebea3755940854d364a7fa2fd25754b6da44
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-28 23:14:17 +0530

    Frontend Setup

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,8 @@ const authRoutes = require('./routes/authRoutes')
 const sessionRoutes = require('./routes/sessionRoutes')
 const questionRoutes = require('./routes/questionRoutes')
 
-
+const { protect } = require("./middlewares/authMiddleware");
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
 
 const app = express();
 
@@ -32,6 +33,8 @@ app.use("/api/sessions", sessionRoutes);
 app.use("/api/questions", questionRoutes);
 
 
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 // serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
