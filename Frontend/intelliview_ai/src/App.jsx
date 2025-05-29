53bf2507fdeee5ef75a30012757808a6ec11d46d
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-05-29 08:38:09 +0530

    utils done

--- Frontend/intelliview_ai/src/App.jsx
+++ Frontend/intelliview_ai/src/App.jsx
@@ -1,37 +1,40 @@
-import React from "react"
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import { Toaster } from "react-hot-toast"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import Login from "./pages/Auth/Login"
-import SignUp from "./pages/Auth/SignUp"
-import LandingPage from "./pages/LandingPage"
-import Dashboard from "./pages/Home/Dashboard"
-import InterviewPrep from "./pages/InterviewPrep/InterviewPrep"
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
+import UserProvider from "./context/userContext";
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          {/* Default Route */}
-          <Route path="/" element={<LandingPage />} />
-          
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            {/* Default Route */}
+            <Route path="/" element={<LandingPage />} />
 
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/interview-prep/:sessionId" element={<InterviewPrep />} />
-        </Routes>
-      </Router>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+              path="/interview-prep/:sessionId"
+              element={<InterviewPrep />}
+            />
+          </Routes>
+        </Router>
 
-      <Toaster 
-      toastOptions={{
-        className: "",
-        style: {
-          fontSize: "13px",
-        },
-      }}
-      />
-    </div>
-  )
-}
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
+    </UserProvider>
+  );
+};
 
-export default App
+export default App;
